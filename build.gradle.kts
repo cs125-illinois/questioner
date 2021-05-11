520c3419ec91174c1d9b6fb57d3f90e90f74ce76
Geoffrey Challen <challen@illinois.edu>
2021-05-10 22:29:48 -0500

    Try again.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,23 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
-    kotlin("jvm") version "1.5.0" apply false
-    kotlin("kapt") version "1.5.0" apply false
+    kotlin("jvm") version "1.4.32" apply false
+    kotlin("kapt") version "1.4.32" apply false
     id("org.jmailen.kotlinter") version "3.4.4" apply false
     id("com.github.ben-manes.versions") version "0.38.0"
 }
 subprojects {
     group = "com.github.cs125-illinois.questioner"
     version = "2021.5.2"
+    tasks.withType<KotlinCompile> {
+        val javaVersion = JavaVersion.VERSION_1_8.toString()
+        sourceCompatibility = javaVersion
+        targetCompatibility = javaVersion
+        kotlinOptions {
+            jvmTarget = javaVersion
+        }
+    }
+    /*
     configurations.all {
         resolutionStrategy {
             force(
@@ -15,6 +26,7 @@ subprojects {
             )
         }
     }
+     */
 }
 allprojects {
     repositories {
