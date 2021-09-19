62fb74fa9d7926e4eb85e707f0a9ad21bac8bcab
Geoffrey Challen <challen@illinois.edu>
2021-09-19 10:45:26 -0500

    Bump version for next changeset.

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 subprojects {
     group = "com.github.cs125-illinois.questioner"
-    version = "2021.9.4"
+    version = "2021.9.5"
     tasks.withType<KotlinCompile> {
         val javaVersion = JavaVersion.VERSION_16.toString()
         sourceCompatibility = javaVersion
