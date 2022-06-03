a7912cf548b67b4aa30631d3a466c7f11b1b7f93
Geoffrey Challen <challen@illinois.edu>
2022-06-03 13:39:36 -0500

    Make memory usage required.

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -55,22 +55,6 @@ tasks.shadowJar {
 }
 application {
     mainClass.set("edu.illinois.cs.cs125.questioner.server.MainKt")
-    val agentJarPath = configurations["runtimeClasspath"].resolvedConfiguration.resolvedArtifacts.find {
-        it.moduleVersion.id.group == "com.beyondgrader.resource-agent"
-    }!!.file.absolutePath
-    applicationDefaultJvmArgs += listOf(
-        "-ea", "--enable-preview", "-javaagent:$agentJarPath",
-        "-Dfile.encoding=UTF-8",
-        "-Xms512m", "-Xmx1G", "-Xss256k", "-XX:+UseZGC", "-XX:ZCollectionInterval=8",
-        "--add-opens", "java.base/java.lang=ALL-UNNAMED",
-        "--add-opens", "java.base/java.util=ALL-UNNAMED",
-        "--add-exports", "jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED",
-        "--add-exports", "jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED",
-        "--add-exports", "jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED",
-        "--add-exports", "jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED",
-        "--add-exports", "jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED",
-        "--add-exports", "java.management/sun.management=ALL-UNNAMED"
-    )
 }
 docker {
     name = "cs125/questioner"
