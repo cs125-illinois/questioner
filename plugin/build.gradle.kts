63dc74c24332e95507a514c8f409588bb246bd0a
Geoffrey Challen <challen@illinois.edu>
2022-07-13 11:16:30 -0500

    Commit for pull.

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -17,7 +17,7 @@ dependencies {
         exclude(module = "kotlin-runtime")
         exclude(module = "kotlin-js")
     }
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
     implementation("com.google.googlejavaformat:google-java-format:1.15.0")
     implementation("com.google.guava:guava:31.1-jre")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.3")
