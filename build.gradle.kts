dced8799f8549a31226b720bba9a3fe3704f1e29
Geoffrey Challen <challen@illinois.edu>
2022-07-23 16:42:53 -0500

    Bump version for next release.

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 subprojects {
     group = "com.github.cs125-illinois.questioner"
-    version = "2022.7.4"
+    version = "2022.7.5"
     tasks.withType<KotlinCompile> {
         kotlinOptions {
             jvmTarget = JavaVersion.VERSION_17.toString()
