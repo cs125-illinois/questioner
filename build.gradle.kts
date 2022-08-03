14bc36eea8ae09c486b6a6d65a343b66e8571ef9
Geoffrey Challen <challen@illinois.edu>
2022-08-03 09:18:25 -0500

    Dependency bumps.

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 subprojects {
     group = "com.github.cs125-illinois.questioner"
-    version = "2022.7.6"
+    version = "2022.8.0"
     tasks.withType<KotlinCompile> {
         kotlinOptions {
             jvmTarget = JavaVersion.VERSION_17.toString()
