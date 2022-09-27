8fe6672da2fb286f969f390ade0a09085b768813
Geoffrey Challen <challen@illinois.edu>
2022-09-27 15:38:10 -0500

    Bump for Jeed dependency.

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 subprojects {
     group = "com.github.cs125-illinois.questioner"
-    version = "2022.9.6"
+    version = "2022.9.7"
     tasks.withType<KotlinCompile> {
         kotlinOptions {
             jvmTarget = JavaVersion.VERSION_17.toString()
